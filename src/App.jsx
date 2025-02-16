dac6ed56c414da628f074cd26001e30c16e69f88
Arthur Lluisma <ajclluisma@gmail.com>
2025-02-16 18:01:59 +0800

    Add basic tailwind styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className="outline-1" onClick={onSquareClick}>
       {value}
     </button>
   );
@@ -39,24 +39,24 @@ export default function Board() {
   }
 
   return (
-    <>
+    <div className="@container w-33">
       <div className="status">{status}</div>
-      <div className="board-row">
+      <div className="grid grid-cols-3 h-11">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <div className="board-row">
+      <div className="grid grid-cols-3 h-11">
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
         <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
         <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <div className="board-row">
+      <div className="grid grid-cols-3 h-11">
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
-    </>
+    </div>
   );
 }
 
